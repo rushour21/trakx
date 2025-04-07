3e6171dde826c68b97fd570783e2fc06da98afd3
rushabh <rushabh.ingle2111@gmail.com>
2025-04-07 17:59:07 +0530

    updated DBconnect

--- server/index.js
+++ server/index.js
@@ -30,10 +30,7 @@ app.use('/api/availability', availabilityRoutes);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
